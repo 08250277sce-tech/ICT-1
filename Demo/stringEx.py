39cd14e2da6e172ab4c998d955cd92461a69b97f
unknown <08250277.sce@rub.edi.bt>
2026-03-18 10:14:48 +0600

    learning string

---
+++ Demo/stringEx.py
@@ -0,0 +1,11 @@
+print("Hello")
+print('Hello')
+multipleLines = '''This is a string
+that spans multiple lines.'''
+print (multipleLines)
+a = 1
+b = 2
+c = a + b
+print(type(c)) 
+c = str() 
+print(type(c)) 
