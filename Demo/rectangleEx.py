33f930e317fa80613e027c3572fe5f7ad24cf945
unknown <08250277.sce@rub.edi.bt>
2026-03-20 15:36:46 +0600

    learning rectangle

---
+++ Demo/rectangleEx.py
@@ -0,0 +1,4 @@
+L = float(input("enter the length of the rectangle: "));
+B = float(input("enter the breadth of the rectangle: "));
+area = L * B;
+print("the area of the rectangle is:" + str(area))
