ce00a4de3bdb42efabda162e2aa6f03cfa6f647a
unknown <08250277.sce@rub.edi.bt>
2026-04-03 15:58:36 +0600

    learning Dictionary

---
+++ Abtract Demo/DictionaryEx.py
@@ -0,0 +1,9 @@
+userDetails = {'Id' : 1, 'userName' : 'Just_me'}
+print(type(userDetails))
+print(userDetails)
+
+location = dict(s = 'samtse', t = 'thimphu', p = 'paro')
+print(location)
+
+print(userDetails['userName'])
+print(location.get('t'))
