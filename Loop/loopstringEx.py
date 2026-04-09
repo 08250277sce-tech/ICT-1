caf466dc941242294feda468276bf8c8e8bc166e
unknown <08250277.sce@rub.edi.bt>
2026-04-09 16:46:32 +0600

    learning of loop string

---
+++ Loop/loopstringEx.py
@@ -0,0 +1,32 @@
+name = input("Enter your name: ")
+for i in name:
+    print(i)
+
+# lterating over list
+li = ["Python Programming", "Python Fundamental", "Python Interview Question"]
+for x in li:
+    print(x)
+
+#range for list
+lenli = len(li)
+for x in range(lenli):
+    print(li[x])
+
+#tuple
+new_tuple = tuple(li)
+for x in new_tuple:
+    print(x)
+
+#range for tuple
+lennew_tuple = len(new_tuple)
+for x in range(lennew_tuple):
+    print(new_tuple[0])
+
+#set
+new_set = set(li)
+for x in new_set:
+    print(x)
+
+
+    
+
