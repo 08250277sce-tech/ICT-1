e230f3a300e6007a53091e263381562955094695
unknown <08250277.sce@rub.edi.bt>
2026-04-10 15:19:24 +0600

    learning loop string

--- Loop/loopstringEx.py
+++ Loop/loopstringEx.py
@@ -27,6 +27,15 @@ new_set = set(li)
 for x in new_set:
     print(x)
 
+#Iterating over set and tuples
+tup = ("John Smith", "Jane Doe", "Alice Johnson")
+for x in tup:
+    print(x)
+
+set1 = {10, 30, 20}
+for x in set1:
+    print(x)
+
 
     
 
