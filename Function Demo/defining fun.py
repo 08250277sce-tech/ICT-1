78ded9bc4cd6b4cc4c812d758e3a98a37090ff9d
unknown <08250277.sce@rub.edi.bt>
2026-04-22 10:02:51 +0600

    defining fun

--- Function Demo/defining fun.py
+++ Function Demo/defining fun.py
@@ -23,6 +23,20 @@ def product_with_parameters(x, y):
 
 product_with_parameters(3, 7)
 
+# return value
+def sum_with_return(x, y):
+    return x + y
+
+print("The sum of 4 and 6 is:", sum_with_return(4, 6))
+
+
+def product_with_return(x, y):
+    return x * y
+
+print("The product of 4 and 6 is:", product_with_return(4, 6))
+
+
+
 
 
 
